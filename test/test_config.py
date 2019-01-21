55cbf226ea9b1033fad149ed8e2dab5b6d205435
Saad Abderrazzaq <24859829+sabderra@users.noreply.github.com>
2019-01-21 00:39:52 -0500

    Travis Integration (#1)
    * travis integration
    
    * travis integration
    
    * travis integration
    
    * simple test for poc
    
    * simple test for poc
    
    * simple test for poc

--- test/test_config.py
+++ test/test_config.py
@@ -1,8 +1,11 @@
-from src.ml_utils.config.config import Config
-from jsonschema import validate
 import json
 import pytest
 import os
+import sys
+from jsonschema import validate
+sys.path.append('../src')
+
+from ml_utils import config
 
 
 def test_schema():
@@ -21,7 +24,7 @@ def test_conf_object_build():
     with open('conf.json') as conf_file:
         conf = json.load(conf_file)
 
-    conf = Config(conf)
+    conf = config.Config(conf)
     c = conf.build()
     assert c.name == "test"
     assert c.description == "Some text"
@@ -33,7 +36,7 @@ def test_conf_object_build_validate():
     with open('conf.json') as conf_file:
         conf = json.load(conf_file)
 
-    conf = Config(conf)
+    conf = config.Config(conf)
     c = conf.build(validate_config=True)
     assert c.name == "test"
     assert c.description == "Some text"
@@ -45,13 +48,13 @@ def test_conf_build_validate():
     with open('conf.json') as conf_file:
         conf = json.load(conf_file)
 
-    conf = Config(conf)
+    conf = config.Config(conf)
     assert conf['name'] == "test"
     assert conf['train']['batch_size'] == 128
 
 
 def test_load_config_file():
-    conf = Config()
+    conf = config.Config()
     c = conf.load('conf.json', validate_config=True)
     assert c.name == "test"
     assert c.description == "Some text"
@@ -59,21 +62,21 @@ def test_load_config_file():
 
 
 def test_expand():
-    conf = Config()
+    conf = config.Config()
     c = conf.load('conf.json', validate_config=True)
     print(*c)
 
 
 def test_save_no_overwrite():
     with pytest.raises(FileExistsError) as e_info:
-        conf = Config()
+        conf = config.Config()
         c = conf.load('conf.json', validate_config=True)
         conf.save('conf.json')
 
 
 def test_save_write():
     test_file = 'conf2.json'
-    conf = Config()
+    conf = config.Config()
     c = conf.load('conf.json', validate_config=True)
     conf.save(test_file, overwrite=True)
     c = conf.load(test_file, validate_config=True)
