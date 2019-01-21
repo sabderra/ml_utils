346baf70e3f0c26c01780b7b83a27117530846aa
saad <saad>
2019-01-20 22:41:30 -0500

    travis integration

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_namespace_packages
 setup(name='ml-utils',
       version='0.1',
       author='Saad Abderrazzaq',
-      author_email='sabderr@example.com',
+      author_email='sadev1344@gmail.com',
       description='Collection of machine learning utilities.',
       url='https://github.com/sabderra/ml_utils',
       install_requires=['jsonschema>=2.6.0', 'cloudant>=2.10.2', 'numpy>=1.15.1'],
